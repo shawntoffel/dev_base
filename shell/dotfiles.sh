53a2616d1f00ddafdab7781ed21dadd65b02904a
Shawn Toffel <shawn.toffel+github@gmail.com>
2018-04-28 14:58:39 -0600

    Update dotfiles.sh

--- shell/dotfiles.sh
+++ shell/dotfiles.sh
@@ -2,10 +2,9 @@
 DOTFILE_DIR="$HOME/.dotfiles"
 DOTFILE_REPO="https://github.com/shawntoffel/dotfiles.git"
 BRANCH="master"
-CONFIG=".install.i3.conf.yaml"
 
 if [ ! -d "$DOTFILE_DIR" ]; then
 	git clone --recursive -b $BRANCH $DOTFILE_REPO $DOTFILE_DIR
 fi
 
-cd $DOTFILE_DIR && ./install $CONFIG
+cd $DOTFILE_DIR && ./install
